a28b640bba44a3f376080ef109524464082491a0
Bruno Leal <bruno.carmo93@gmail.com>
2019-04-18 16:46:45 -0300

    Atualização na bcpro.html

--- app/static/js/bcpro.js
+++ app/static/js/bcpro.js
@@ -1,8 +1,10 @@
 function pctTrade(){
     if($(document.getElementById("collapseTrade")).hasClass('show')==false){
         $('#iconTrade').addClass('fa-rotate-180');
+        
     } else{
         $('#iconTrade').removeClass('fa-rotate-180');
+        
         
     }
 }
@@ -41,4 +43,21 @@ function pctAnalises(){
 
 $(document).ready(function() {
         $('.fixed').addClass('is-sticky');
+        var url_atual = window.location.href;
+        /*if(url_atual.match(/Trade/)){
+            document.getElementById("pctTrade").click();
+        }*/
+        if(url_atual.match(/Conjuntura/)){
+            document.getElementById("pctConjuntura").click();
+        }
+        if(url_atual.match(/Mercados/)){
+            document.getElementById("pctMercados").click();
+        }
+        if(url_atual.match(/News/)){
+            document.getElementById("pctNews").click();
+        }
+        if(url_atual.match(/Analises/)){
+            document.getElementById("pctAnalises").click();
+        }
+       
     });
